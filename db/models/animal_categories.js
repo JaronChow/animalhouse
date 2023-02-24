d4e9e5d0467e8a950742ec53520083a2734b26a7
Adam Lu <adamlu09@gmail.com>
2023-02-24 12:22:04 -0800

    created functions for categories, customer sales, and sales items. Also changed DB name in the client to match project name.

---
+++ db/models/animal_categories.js
@@ -0,0 +1,24 @@
+const client = require('../client');
+
+async function createCategory({ category_name }) {
+  /* this adapter should fetch a list of users from your db */
+  try {
+    const { rows: [ category ] } = await client.query(
+      `
+        INSERT INTO animal_categories(category_name)
+        VALUES($1)
+        ON CONFLICT DO NOTHING
+        RETURNING *;
+      `
+    , [category_name]);
+
+    return category;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  // add your database adapter fns here
+  createCategory
+};
---
+++ db/models/customer_sale.js
@@ -0,0 +1,55 @@
+const client = require('../client');
+
+async function createSale({
+  total_item_amount,
+  shipping_fee,
+  tax_amount,
+  sales_total_amount,
+  sales_date
+}) {
+  /* this adapter should fetch a list of users from your db */
+  try {
+    const { rows: [ sale ] } = await client.query(
+      `
+        INSERT INTO customers_sale(total_item_amount, shipping_fee, tax_amount, sales_total_amount, sales_date)
+        VALUES($1, $2, $3, $4, $5)
+        ON CONFLICT DO NOTHING
+        RETURNING *;
+      `
+    , [total_item_amount, shipping_fee, tax_amount, sales_total_amount, sales_date]);
+
+    return sale;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// async function addSaleToSaleItem({
+//   total_item_amount,
+//   shipping_fee,
+//   tax_amount,
+//   sales_total_amount,
+//   sales_date
+// }) {
+//   try {
+//     const { rows: [ sale ] } = await client.query(
+//       `
+//         INSERT INTO customers_sale(total_item_amount, shipping_fee, tax_amount, sales_total_amount, sales_date)
+//         VALUES($1, $2, $3, $4, $5)
+//         ON CONFLICT DO NOTHING
+//         RETURNING *;
+//       `
+//     , [total_item_amount, shipping_fee, tax_amount, sales_total_amount, sales_date]);
+
+//     return sale;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+module.exports = {
+  // add your database adapter fns here
+  createSale,
+  // addSaleToSaleItem
+
+};
