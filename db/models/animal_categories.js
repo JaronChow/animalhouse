7465a1ee1943ba7e63ee7254e94503af0e88baa4
Winnie Liu <pewinonlyy@gmail.com>
2023-02-25 22:35:06 -0800

    generated functions

--- db/models/animal_categories.js
+++ db/models/animal_categories.js
@@ -3,14 +3,11 @@ const client = require('../client');
 async function createCategory({ category_name }) {
   /* this adapter should fetch a list of users from your db */
   try {
-    const { rows: [ category ] } = await client.query(
-      `
-        INSERT INTO animal_categories(category_name)
-        VALUES($1)
-        ON CONFLICT(category_name) DO NOTHING
-        RETURNING *;
-      `
-    , [category_name]);
+    const { rows: [ category ] } = await client.query(`
+      INSERT INTO animal_categories(category_name)
+      VALUES($1)
+      RETURNING *;
+    `, [category_name]);
 
     return category;
   } catch (error) {
@@ -18,9 +15,37 @@ async function createCategory({ category_name }) {
   }
 }
 
+async function getAllCategories() {
+  // select and return an array of all activities
+  try {
+    const { rows } = await client.query(`
+      SELECT * 
+      FROM animal_categories;
+    `);
 
+    return rows;
+  } catch (error) {
+    console.log("Error getting categories!")
+  }
+}
+
+async function getCategoryById(id) {
+  try{
+    const { rows: [ category ] } = await client.query(`
+      SELECT * 
+      FROM animal_categories
+      WHERE id =${id};
+    `);
+
+    return category;
+  } catch (error) {
+    console.log("Error getting categories by id!")
+  }
+}
 
 module.exports = {
   // add your database adapter fns here
-  createCategory
+  createCategory,
+  getAllCategories,
+  getCategoryById
 };
--- db/models/animals.js
+++ db/models/animals.js
@@ -1,24 +1,90 @@
 const client = require('../client');
 
-module.exports = {
-  createAnimal,
-}
-
-
 async function createAnimal({ categoryId, breed_name, image_url, description, inventory_count, price, gender }) {
   try {
-    const {
-      rows: [animal],
-    } = await client.query(
-      `
-        INSERT INTO animals ( "categoryId", breed_name, image_url, description, inventory_count, price, gender )
+    const { rows: [ animal ]} = await client.query(`
+        INSERT INTO animals ("categoryId", breed_name, image_url, description, inventory_count, price, gender)
         VALUES ($1,$2,$3,$4,$5,$6,$7)
-        RETURNING *
-      ;`,
-      [categoryId, breed_name, image_url, description, inventory_count, price, gender]
+        RETURNING *;
+        `, [categoryId, breed_name, image_url, description, inventory_count, price, gender]
     );
     return animal;
   } catch (error) {
     console.log(error);
   }
+}
+
+async function getAllAnimals() {
+  // select and return an array of all activities
+  try {
+    const { rows } = await client.query(`
+      SELECT * 
+      FROM animals;
+    `);
+
+    return rows;
+  } catch (error) {
+    console.log("Error getting categories!")
+  }
+}
+
+async function getAnimalById(id) {
+  try{
+    const { rows: [ animal ] } = await client.query(`
+      SELECT * 
+      FROM animals
+      WHERE id =${id};
+    `);
+
+    return animal;
+  } catch (error) {
+    console.log("Error getting categories by id!")
+  }
+}
+
+async function getAnimalByGender(id, gender) {
+  try{
+    const { rows: [ animal ] } = await client.query(`
+      SELECT * 
+      FROM animals
+      WHERE id =${id} AND gender =${gender};
+    `);
+
+    return animal;
+  } catch (error) {
+    console.log("Error getting categories by id!")
+  }
+}
+
+async function updateAnimal({ id, ...fields }) {
+  // build the set string
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+      const { rows: [ animal ] } = await client.query(`
+        UPDATE animals
+        SET ${ setString }
+        WHERE id=${id}
+        RETURNING *;
+      `, Object.values(fields));
+
+  return animal;
+  } catch (error) {
+    console.log("Error updating the animal!")
+  }
+}
+
+module.exports = {
+  createAnimal,
+  getAllAnimals,
+  getAnimalById,
+  getAnimalByGender,
+  updateAnimal
 }
