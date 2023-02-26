4ec1de7989e27ec4b0b71299605a14d3603f7555
JaronChow <jchow826@gmail.com>
2023-02-25 17:48:11 -0800

    Updated table creation

--- db/models/animals.js
+++ db/models/animals.js
@@ -5,17 +5,17 @@ module.exports = {
 }
 
 
-async function createAnimal({ breed_name, image_url, category_id, description, inventory_count, price, gender }) {
+async function createAnimal({ categoryId, breed_name, image_url, description, inventory_count, price, gender }) {
   try {
     const {
       rows: [animal],
     } = await client.query(
       `
-        INSERT INTO animals ( breed_name, image_url, "category_id", description, inventory_count, price, gender )
+        INSERT INTO animals ( "categoryId", breed_name, image_url, description, inventory_count, price, gender )
         VALUES ($1,$2,$3,$4,$5,$6,$7)
         RETURNING *
       ;`,
-      [breed_name, image_url, category_id, description, inventory_count, price, gender]
+      [categoryId, breed_name, image_url, description, inventory_count, price, gender]
     );
     return animal;
   } catch (error) {
