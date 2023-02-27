812eae67e443a2eebacd6752923a2e5f0151aa1a
Winnie Liu <pewinonlyy@gmail.com>
2023-02-26 23:08:51 -0800

    updated api/animals and api/categories

--- api/animals.js
+++ api/animals.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { requireAdmin } = require('./utils');
 const {
-    getAllAnimals
+    getAllAnimals,
+    createAnimal,
+    getAnimalByGender,
+    updateAnimal
 } = require('../db');
 
 // GET /api/animals
@@ -16,19 +19,35 @@ router.post('/', requireAdmin, async (req, res, next) => {
     const { categoryId, breed_name, image_url, description, inventory_count, price, gender } = req.body;
   
     try {
-        if (!req.admin) {
-            res.send({
-                error: "Error",
-                name: "UnauthorizedUser",
-                message: "You must be logged in to perform this action"
-            })
-        } else {
-            const newRoutine = await createRoutine({ creatorId:req.user.id, isPublic, name, goal });
-            res.send(newRoutine);
-        }
+        const newAnimal = await createAnimal({ categoryId, breed_name, image_url, description, inventory_count, price, gender });
+        res.send(newAnimal);
     } catch(error) {
         next(error)
     } 
 });
+
+// PATCH /api/animals/:id
+router.patch("/:id", requireAdmin, async (req, res, next) => {
+    const { id } = req.params;
+    const { categoryId, breed_name, image_url, description, inventory_count, price, gender } = req.body;
+
+    try {
+        const animal = await getAnimalByGender(id, gender);
+        
+        if (animal.creatorId === req.admin.id) {
+            const updateAnimal = await updateAnimal({ id, categoryId, breed_name, image_url, description, inventory_count, price, gender });
+            res.send(updateAnimal)
+        } else {
+            res.status(403).send({
+                error: "Error",
+                name: "UnauthorizedAdminError",
+                message: `Admin ${req.admin.username} is not allowed to update ${animal.breed_name}`
+            })
+        }
+    } catch (error) {
+        console.log(error);
+        next(error);
+    }
+})
 
 module.exports = router;
