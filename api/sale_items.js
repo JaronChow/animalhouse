d05ec4248e61c05a68690aa6b30cda33a547040e
Adam Lu <adamlu09@gmail.com>
2023-02-28 21:00:23 -0800

    deleted getSaleItemByOrderId from db/sale_items, and changed api router.get to getAllSaleItemsByCustomerId

--- api/sale_items.js
+++ api/sale_items.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { requireAdmin } = require('./utils');
 const {
-  getSaleItemByOrderId,
+  getAllSalesItemsByCustomerId,
   createSaleItem
 } = require('../db');
 
@@ -10,7 +10,7 @@ router.get('/:orderId/sale_items', async (req, res, next) => {
   const id = req.params.orderId;
 
   try {
-    const saleItem = await getSaleItemByOrderId({ id });
+    const saleItem = await getAllSalesItemsByCustomerId({ id });
 
     if (saleItem) {
       res.send(saleItem);
