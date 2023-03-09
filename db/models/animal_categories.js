64afd630903ac1fb8df8ce79bdf4b3822004fef8
Winnie Liu <pewinonlyy@gmail.com>
2023-03-08 22:56:12 -0800

    fixed Edit and Delete errors

--- db/models/animal_categories.js
+++ db/models/animal_categories.js
@@ -43,9 +43,34 @@ async function getCategoryById(id) {
   }
 }
 
+async function updateCategory({ id, ...fields }) {
+  // build the set string
+  const setString = Object.keys(fields).map(
+    (key, index) => `"${ key }"=$${ index + 1 }`
+  ).join(', ');
+
+  // return early if this is called without fields
+  if (setString.length === 0) {
+    return;
+  }
+
+  try {
+      const { rows: [ category ] } = await client.query(`
+        UPDATE animal_categories
+        SET ${ setString }
+        WHERE id=${id}
+        RETURNING *;
+      `, Object.values(fields));
+
+  return category;
+  } catch (error) {
+    console.log("Error updating the category!")
+  }
+}
+
 async function deleteCategory(id) {
   const { rows: [category] } = await client.query(`
-  DELETE FROM categories
+  DELETE FROM animal_categories
   WHERE id = $1
   RETURNING *;
   `, [id]);
@@ -58,5 +83,6 @@ module.exports = {
   createCategory,
   getAllCategories,
   getCategoryById,
+  updateCategory,
   deleteCategory
 };
