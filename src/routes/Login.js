d9c5993b0bfafa22b579f0c81b6dad0785428919
Winnie Liu <pewinonlyy@gmail.com>
2023-03-16 00:34:54 -0700

    fixed margin and border issue

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -5,7 +5,7 @@ import { Tab, Nav } from 'react-bootstrap';
 const Login = () => {
     return(
         <div style={{ maxWidth: '800px', margin: '60px auto 0px' }}>
-            <Tab.Container defaultActiveKey="tab1" border>
+            <Tab.Container defaultActiveKey="tab1">
                 <Nav variant="tabs" fill>
                     <Nav.Item>
                         <Nav.Link eventKey="tab1">Log In as Admin</Nav.Link>
@@ -14,7 +14,7 @@ const Login = () => {
                         <Nav.Link eventKey="tab2">Log In as Customer</Nav.Link>
                     </Nav.Item>
                 </Nav>
-                <div style={{ height: '400px', border: '1px solid #dee2e6', borderTop: 'none', borderRadius: '5px' }}>
+                <div style={{ height: '410px', border: '1px solid #dee2e6', borderTop: 'none', borderRadius: '5px', padding: '10px' }}>
                 <Tab.Content>
                     <Tab.Pane eventKey="tab1"><AdminLogin /></Tab.Pane>
                     <Tab.Pane eventKey="tab2"><CustomerLogin /></Tab.Pane>
