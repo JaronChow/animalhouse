40c778acbb8cc18c4595e208b90fd9ff4f546ed0
Adam Lu <adamlu09@gmail.com>
2023-03-19 20:41:55 -0700

    fixed new categories and new animals, so that new data loads after creating without having to refresh browser

--- src/components/NewCategory.js
+++ src/components/NewCategory.js
@@ -29,7 +29,7 @@ const NewCategory = () => {
                 }
             })
 
-            setCategories(updateCategory);
+            setCategories([...updateCategory, response.data]);
             navigate('/categories');
         }
     }
