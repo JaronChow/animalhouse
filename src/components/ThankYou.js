592644c000469c5bb44c08dd5e033d2fbc2ceb4c
Adam Lu <adamlu09@gmail.com>
2023-03-22 22:50:27 -0700

    edited thankyou

--- src/components/ThankYou.js
+++ src/components/ThankYou.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { Card } from "react-bootstrap";
+import { Card, Container, Image } from "react-bootstrap";
 
 const ThankYou = () => {
 
@@ -15,8 +15,8 @@ const ThankYou = () => {
     return (
         <Container className="mt-4 d-flex justify-content-center" style={{ maxWidth: '800px' }}>
             <div className="d-flex flex-wrap justify-content-center">
-                <Card key={id} style={{ width: '18rem' }} className="mb-3 me-3">
-                    <Card.Body>Thank you for your order!</Card.Body>
+                <Card className="mt-4">
+                    <Card.Body><Image src={"https://marketplace.canva.com/EAEaIMAz-WI/2/0/1600w/canva-beige-minimal-thank-you-card-jOZOzZod2TE.jpg"} style={{ width: '600px', height: 'auto' }} /></Card.Body>
                 </Card>
             </div> 
         </Container>
